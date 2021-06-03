714f400b7460c2169f72e76acf859d0f57101446
luisNvasArg <lpipnavas@gmail.com>
2021-06-03 00:07:03 -0300

    Creado la interfaz

---
+++ js/app.js
@@ -0,0 +1,19 @@
+document.body.onload=()=>{
+    document.getElementById("fondo").innerHTML=` <div id="1" class="cuadro azul">1</div>
+    <div id="2" class="cuadro azul">2</div>
+    <div id="3" class="cuadro azul">3</div>
+    <div id="4" class="cuadro azul">4</div>
+    <div id="5" class="cuadro verde">5</div>
+    <div id="6" class="cuadro verde">6</div>
+    <div id="7" class="cuadro verde">7</div>
+    <div id="8" class="cuadro verde">8</div>
+    <div id="9" class="cuadro rojo">9</div>
+    <div id="10" class="cuadro rojo">10</div>
+    <div id="11" class="cuadro rojo">11</div>
+    <div id="12" class="cuadro rojo">12</div>
+    <div id="13" class="cuadro amarillo">13</div>
+    <div id="14" class="cuadro amarillo">14</div>
+    <div id="15" class="cuadro amarillo">15</div>
+    <div id="16" class="cuadro negro">16</div>`;
+      
+};
